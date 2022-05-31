5b8a87b0831db223e40b6a99341d8a7f5feda9f2
sharsora <sharsora@illumina.com>
2022-05-31 21:29:05 +0100

    Updated Unit tests

--- MusicApi.Tests/TracksControllerUnitTest.cs
+++ MusicApi.Tests/TracksControllerUnitTest.cs
@@ -1,7 +1,11 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using MusicApi.Controllers;
 using MusicApi.Interfaces;
+using MusicApi.Models;
+using MusicApi.Tests.Utils;
 
 namespace MusicApi.Tests
 {
@@ -11,11 +15,128 @@ namespace MusicApi.Tests
         private readonly Mock<ILogger<TracksController>> loggerStub = new(); // Mock Stub of logger dependency
         private readonly Mock<ITracksService> tracksServiceStub = new(); // Mock Stub of pokemonService dependency
         #endregion
+       
+        [Fact]
+        public void FindTracksByWord_WithExpectedWord_ReturnsExpectedResult()
+        {
+            //Arrange
+            var trackListMock = DataModels.GetSampleTracksDtoList();
+            tracksServiceStub.Setup(tracks => tracks.ListByWordAsync(It.IsAny<string>())).ReturnsAsync(trackListMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+
+            // Act 
+            var actionResult = controller.FindTracksByWord(It.IsAny<string>());
+
+            // Assert
+            actionResult.Result.Should().BeOfType<ActionResult<TrackDTO>>();
+        }
+
 
         [Fact]
-        public void Test1()
+        public void FindTracksByWord_WithExpectedWord_ReturnsOk()
         {
+            //Arrange
+            var trackListMock = DataModels.GetSampleTracksDtoList();
+            tracksServiceStub.Setup(tracks => tracks.ListByWordAsync(It.IsAny<string>())).ReturnsAsync(trackListMock);
 
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new OkObjectResult(trackListMock));
+
+            // Act 
+            var actionResult = controller.FindTracksByWord(It.IsAny<string>());
+
+            // Assert
+            actionResult.Result.Should().BeEquivalentTo(actionResultShouldbe);
+        }
+
+        [Fact]
+        public void FindTracksByWord_WithNonExistentWord_ReturnsNotFound()
+        {
+            //Arrange
+            List<TrackDTO> trackListMock = null; // initialise empty list to denote empty result from TracksService
+            tracksServiceStub.Setup(tracks => tracks.ListByWordAsync(It.IsAny<string>())).ReturnsAsync(trackListMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new NotFoundResult());
+
+            // Act 
+            var actionResult = controller.FindTracksByWord(It.IsAny<string>());
+
+            // Assert
+            actionResult.Result.Should().BeEquivalentTo(actionResultShouldbe);
+        }
+
+        [Fact]
+        public void FindTracksByWord_WithSpace_ReturnsBadRequest()
+        {
+            //Arrange
+            var trackListMock = DataModels.GetSampleTracksDtoList();
+            tracksServiceStub.Setup(tracks => tracks.ListByWordAsync(It.IsAny<string>())).ReturnsAsync(trackListMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new BadRequestResult());
+
+            // Act 
+            var actionResult = controller.FindTracksByWord(" ");
+
+            // Assert
+            actionResult.Result.Should().BeEquivalentTo(actionResultShouldbe);
+
+        }
+
+        [Fact]
+        public void GetTrackCountByWord_WithExpectedWord_ReturnsExpectedResult()
+        {
+            //Arrange
+            int trackCountMock = 3; // Picking the value count of the word "Come" according to the sampledata.json 
+            tracksServiceStub.Setup(tracks => tracks.GetTrackCountByWord(It.IsAny<string>())).ReturnsAsync(trackCountMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new OkObjectResult($"{{\"count\":{trackCountMock}}})"));
+
+            // Act 
+            var actionResult = controller.GetTrackCountByWord("Come");
+
+            // Assert
+            actionResult.Result.Should().BeOfType<ActionResult<TrackDTO>>();
+        }
+
+        [Fact]
+        public void GetTrackCountByWord_WithNonExistentWord_ReturnsNotFound()
+        {
+            //Arrange
+            int trackCountMock = 0;  
+            tracksServiceStub.Setup(tracks => tracks.GetTrackCountByWord(It.IsAny<string>())).ReturnsAsync(trackCountMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new NotFoundResult());
+
+            // Act 
+            var actionResult = controller.GetTrackCountByWord(It.IsAny<string>());
+
+            // Assert
+            actionResult.Result.Should().BeEquivalentTo(actionResultShouldbe);
+        }
+
+        [Fact]
+        public void GetTrackCountByWord_WithSpace_ReturnsBadRequest()
+        {
+            //Arrange
+            int trackCountMock = 0;
+            tracksServiceStub.Setup(tracks => tracks.GetTrackCountByWord(It.IsAny<string>())).ReturnsAsync(trackCountMock);
+
+            var controller = new TracksController(tracksServiceStub.Object, loggerStub.Object);
+            var actionResultShouldbe = new ActionResult<TrackDTO>(new BadRequestResult());
+
+            // Act 
+            var actionResult = controller.GetTrackCountByWord(" ");
+
+            // Assert
+            actionResult.Result.Should().BeEquivalentTo(actionResultShouldbe);
         }
     }
 }
---
+++ MusicApi.Tests/Utils/DataModels.cs
@@ -0,0 +1,44 @@
+﻿using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MusicApi.Tests.Utils
+{
+    static class DataModels
+    {
+        private static string LoadJsonStringFromFile(string filepath)
+        {
+            try
+            { 
+                var path = Path.GetFullPath(filepath);
+                var json =  JArray.Parse(File.ReadAllText(path));
+                return json.ToString();
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+        private static List<TrackDTO>? GetTracksDtoFromJson(string jsonString)
+        {
+            var tracks = JsonConvert.DeserializeObject<List<TrackDTO>>(jsonString);
+
+            return tracks;
+        }
+
+        public static List<TrackDTO> GetSampleTracksDtoList()
+        {
+   
+            var jsonString = LoadJsonStringFromFile(@"../../../sampledata.json");
+            var tracks = GetTracksDtoFromJson(jsonString);
+
+            return tracks;
+        }
+    }
+}
